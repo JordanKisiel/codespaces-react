0f9a30373276c6531737b40660e4d63a553af7ed
Jordan Kisiel <jordan.kisiel@gmail.com>
2023-01-14 01:39:53 +0000

    Added Router capabilities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Booklist from './components/Booklist';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <nav>
+        <ul>
+          <li><Link to="/" >Home</Link></li>
+          <li><Link to="/books" >Booklist</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/books' element={<Booklist />} />
+        <Route />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Booklist.js
@@ -0,0 +1,13 @@
+export default function Booklist(){
+
+    return (
+        <>
+            <h1>Books</h1>
+            <ul>
+                <li>Moby Dick</li>
+                <li>Confederacy of Dunces</li>
+                <li>Watership Down</li>
+            </ul>    
+        </>
+    )
+}
